51a354fc5d640cfea6c99b67c7a2357dc38e2fef
Yehia <yehia.elhady@incorta.com>
2021-07-27 04:21:52 +0200

    Initial commit

---
+++ gradle-basic/src/main/com/example/gradle/Main.java
@@ -0,0 +1,12 @@
+package com.example.gradle;
+
+import java.util.Arrays;
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String... args) {
+		System.out.println(StringUtils.swapCase(Arrays.toString(args)));
+	}
+
+}
---
+++ gradle-basic/src/test/com/example/gradle/MainTest.java
@@ -0,0 +1,13 @@
+package com.example.gradle;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        assertTrue(1 == 1);
+    }
+}
